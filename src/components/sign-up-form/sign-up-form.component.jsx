d2101db77e0b1cfdf7f5eb661f83fe22b584c66b
Joeld1027 <56660581+Joeld1027@users.noreply.github.com>
2022-07-06 20:03:05 -0500

    created signup and signin button component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import {
+	createAuthUserWithEmailAndPassword,
+	createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+import "./sign-up-form.styles.scss";
+
+const defaultFormFields = {
+	displayName: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+};
+const testFields = ["displayName", "email", "password", "confirmPassword"];
+
+const SignUpForm = () => {
+	const [formFields, setFormFields] = useState(defaultFormFields);
+	const { displayName, email, password, confirmPassword } = formFields;
+
+	const resetFormFields = () => {
+		setFormFields(defaultFormFields);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		if (password !== confirmPassword) {
+			alert("passwords dont match");
+			return;
+		}
+		try {
+			const { user } = await createAuthUserWithEmailAndPassword(
+				email,
+				password
+			);
+
+			await createUserDocumentFromAuth(user, { displayName });
+			resetFormFields();
+		} catch (err) {
+			if (err.code === "auth/email-already-in-use") {
+				alert("Cannot create user. Email already in use.");
+			}
+			console.log(err, "Something went wrong while creating user");
+		}
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormFields({ ...formFields, [name]: value });
+	};
+
+	return (
+		<div className="sign-up-container">
+			<h2>Don't have an account?</h2>
+			<span>Sign up with your email and password</span>
+			<form onSubmit={handleSubmit}>
+				{testFields.map((field) => {
+					return (
+						<FormInput
+							key={field}
+							label={
+								field === "confirmPassword"
+									? "CONFIRM PASSWORD"
+									: field.toUpperCase()
+							}
+							type={field === "confirmPassword" ? "password" : field}
+							required
+							onChange={handleChange}
+							name={field}
+							value={formFields[field]}
+						/>
+					);
+				})}
+				<Button type="submit">Sign Up</Button>
+			</form>
+		</div>
+	);
+};
+
+export default SignUpForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
 	signInWithRedirect,
 	signInWithPopup,
 	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -21,17 +22,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
 	prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+	signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+	signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+	userAuth,
+	additionalInformation = {}
+) => {
+	if (!userAuth) return;
 	const userDocRef = doc(db, "users", userAuth.uid);
 	console.log(userDocRef);
 	const userSnapshot = await getDoc(userDocRef);
@@ -45,10 +53,16 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 				displayName,
 				email,
 				createdAt,
+				...additionalInformation,
 			});
 		} catch (err) {
 			console.log("error creating user", err);
 		}
 	}
 	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+	return await createUserWithEmailAndPassword(auth, email, password);
 };
